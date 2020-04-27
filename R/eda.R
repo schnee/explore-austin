07d3c50c8276332aed18199f151c683df2adbb84
schnee <schneeman@gmail.com>
2020-04-27 18:34:25 -0500

    density changes

--- R/eda.R
+++ R/eda.R
@@ -27,3 +27,28 @@ survey_df %>% mutate(`Do you receive free/reduced lunch?` = if_else(`Do you rece
   geom_col() + theme_ipsum() +
   theme(legend.position = "none")
 
+social_cap <- survey_df %>% select(name, knows_how_to_fix_a_car:owns_a_car)
+
+cap_long <- social_cap %>%
+  pivot_longer(knows_how_to_fix_a_car:owns_a_car, names_to = "cap_measure", values_to = "value") %>%
+  mutate(value = na_if(value, "Yes | No"),
+         value = na_if(value,  "IDK"),
+         value = na_if(value, "I don't know"),
+         value = na_if(value, "`Yes"))
+
+cap_long %>% count(cap_measure,value) %>%
+  pivot_wider(id_cols = "cap_measure", names_from = "value", values_from = "n") %>%
+  arrange(desc(No))
+
+
+cap_long %>%
+  mutate(numeric_value = if_else(value == "No", 0, 1)) %>%
+  group_by(name) %>% tally(numeric_value)
+
+cap_density <- cap_long %>%
+  mutate(numeric_value = if_else(value == "No", 0, 1)) %>%
+  group_by(name) %>% tally(numeric_value) %>%
+  ggplot(aes(x=n)) +
+  geom_boxplot(fill = base_color, color=base_color, alpha=0.5) +
+  theme_ipsum()
+
